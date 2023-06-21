0f634bee600490252bf939a0ebebd20b2dfcfc5a
Ilya Smirkin <ilyasmirkin200137@gmail.com>
2023-06-21 23:17:55 +0300

    Update testmem64.c
    added record pattern selection

--- testmem64.c
+++ testmem64.c
@@ -8,15 +8,18 @@
 #define DEFAULT_MEMSIZE 64
 #define DEFAULT_ITERATIONS 1
 #define DEFAULT_PGSIZE (1024 * 1024)
+#define DEFAULT_VERBOSE false
+#define DEFAULT_PATTERN 'Z'
+#define USAGE_OUT "Usage: %s [-m memory_size] [-i iterations] [-p page_size] [-c pattern] [-v] [-h]\n\r"
 
-void test_memory(char* mem, int size) {
+static inline void test_memory(char* mem, int size, char pattern) {
     for(int i = 0; i < size; i++) {
-        *(mem+i) = 'Z';
+        *(mem+i) = pattern;
     }
 
     bool equal = true;
     for (int i = 0; i < size-1; i++) {
-        equal &= (*(mem+i) == 'Z');
+        equal &= (*(mem+i) == pattern);
     }
     if (!equal) {
         printf("error detected!\n\r");
@@ -29,10 +32,11 @@ int main(int argc, char* argv[]) {
     int memsize = DEFAULT_MEMSIZE;
     int iterations = DEFAULT_ITERATIONS;
     int pgsize = DEFAULT_PGSIZE;
-    bool verbose = false;
+    char pattern = DEFAULT_PATTERN;
+    bool verbose = DEFAULT_VERBOSE;
 
     int opt;
-    while ((opt = getopt(argc, argv, "m:i:p:vh")) != -1) {
+    while ((opt = getopt(argc, argv, "m:i:p:c:vh")) != -1) {
         switch (opt) {
             case 'm':
                 memsize = atoi(optarg);
@@ -44,13 +48,16 @@ int main(int argc, char* argv[]) {
                 pgsize = atoi(optarg);
                 break;    
             case 'h':
-                printf("Usage: %s [-m memory_size] [-i iterations] [-p page_size] [-v] [-h]\n\r", argv[0]);
+                printf(USAGE_OUT, argv[0]);
                 return 0;
             case 'v':
                 verbose = true;
                 break; 
+            case 'c':
+                pattern = *optarg;
+                break;
             default:
-                fprintf(stderr, "Usage: %s [-m memory_size] [-i iterations] [-p page_size] [-v] [-h]\n\r", argv[0]);
+                fprintf(stderr, USAGE_OUT, argv[0]);
                 return 1;
         }
     }
@@ -79,11 +86,17 @@ int main(int argc, char* argv[]) {
 
     printf("Memory successfully allocated (%d pages).\n\r", memsize);
 
+    if(pattern != DEFAULT_PATTERN) {
+        printf("Using record pattern '%c'\n\r", pattern);
+    } else {
+        printf("Using default record pattern\n\r");
+    }
+
     for(int n = 0; n < memsize; n++) {
         unsigned char* ptr = ptrs[n];
         printf("Testing memory at %p (%d page)... ", (void*) ptr, n);
         for(int iter = 0; iter < iterations; iter++) {
-            test_memory((char*)ptr, pgsize);
+            test_memory((char*)ptr, pgsize, pattern);
         }
     }
     
