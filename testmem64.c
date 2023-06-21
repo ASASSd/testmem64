52dda056aaa085b134f66bb474d70cf9dadf343a
Ilya Smirkin <ilyasmirkin200137@gmail.com>
2023-06-22 01:11:57 +0300

    Update testmem64.c
    split program into header and code

--- testmem64.c
+++ testmem64.c
@@ -4,15 +4,9 @@
 #include <string.h>
 #include <getopt.h>
 #include <errno.h>
+#include "testmem64.h"
 
-#define DEFAULT_MEMSIZE 64
-#define DEFAULT_ITERATIONS 1
-#define DEFAULT_PGSIZE (1024 * 1024)
-#define DEFAULT_VERBOSE false
-#define DEFAULT_PATTERN 'Z'
-#define USAGE_OUT "Usage: %s [-m memory_size] [-i iterations] [-p page_size] [-c pattern] [-v] [-h]\n\r"
-
-static inline void test_memory(char* mem, int size, char pattern) {
+void test_memory(char* mem, int size, char pattern) {
     for(int i = 0; i < size; i++) {
         *(mem+i) = pattern;
     }
