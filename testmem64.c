64d24fce68edbf52b6bf818f88103c5ec724b6d0
Smirkin Ilya <ilyasmirkin200137@gmail.com>
2023-12-16 15:17:27 +0300

    rewrote datatypes for variables (for better cross-platform compatibility)

--- testmem64.c
+++ testmem64.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
 #include <getopt.h>
 #include <errno.h>
 #include "testmem64.h"
 
-void test_memory(char* mem, int size, char pattern) {
-    for (int i = 0; i < size; i++) {
+void test_memory(uint8_t* mem, uint32_t size, uint8_t pattern) {
+    for (uint32_t i = 0; i < size; i++) {
         *(mem+i) = pattern;
     }
 
-    bool equal = true;
-    for (int i = 0; i < size-1; i++) {
-        equal &= (*(mem+i) == pattern);
+    uint8_t equal = 0xFF;
+    for (uint32_t i = 0; i < size-1; i++) {
+        equal &= eq(*(mem+i), pattern);
     }
     if (!equal) {
         printf("error detected!\n\r");
@@ -23,12 +23,12 @@ void test_memory(char* mem, int size, char pattern) {
 }
 
 int main(int argc, char* argv[]) {
-    int memsize = DEFAULT_MEMSIZE;
-    int iterations = DEFAULT_ITERATIONS;
-    int pgsize = DEFAULT_PGSIZE;
-    char pattern = DEFAULT_PATTERN;
-    bool verbose = DEFAULT_VERBOSE;
-    bool stress_mode = DEFAULT_STRESSMODE;
+    uint32_t memsize = DEFAULT_MEMSIZE;
+    uint32_t iterations = DEFAULT_ITERATIONS;
+    uint32_t pgsize = DEFAULT_PGSIZE;
+    uint8_t pattern = DEFAULT_PATTERN;
+    uint8_t verbose = DEFAULT_VERBOSE;
+    uint8_t stress_mode = DEFAULT_STRESSMODE;
 
     int opt;
     while ((opt = getopt(argc, argv, "m:i:p:c:vhs")) != -1) {
@@ -43,13 +43,13 @@ int main(int argc, char* argv[]) {
                 pgsize = atoi(optarg);
                 break;
             case 'v':
-                verbose = true;
+                verbose = 0xFF;
                 break;
             case 'h':
                 printf(USAGE_OUT, argv[0]);
                 return 0;
             case 's':
-                stress_mode = true;
+                stress_mode = 0xFF;
                 break;
             case 'c':
                 pattern = *optarg;
@@ -61,22 +61,22 @@ int main(int argc, char* argv[]) {
     }
 
     printf("Allocating %d testing pages, %d bytes per page\n\r", memsize, pgsize);
-    unsigned char* ptrs[memsize];
-    for (int n = 0; n < memsize; n++) {
-        unsigned char* ptr = (unsigned char*) malloc(pgsize);
+    uint8_t* ptrs[memsize];
+    for (uint32_t n = 0; n < memsize; n++) {
+        uint8_t* ptr = (uint8_t*) malloc(pgsize);
 
         if (ptr == 0x0) {
             printf("Error: null pointer returned (Out of heap?)\n\r");
-            for (int p = 0; p <= n; p++) {
+            for (uint32_t p = 0; p <= n; p++) {
                 free(ptrs[p]);
             }
-            return -ENOMEM;
+            return ENOMEM;
         }
         if (verbose) {
             printf("\rAllocating memory at %p (%d page)... ", (void*) ptr, n);
         }
 
-        for (int i = 0; i < pgsize; i++) {
+        for (uint32_t i = 0; i < pgsize; i++) {
                 *(ptr+i) = 0x0;
         }
         ptrs[n] = ptr;        
@@ -94,12 +94,12 @@ int main(int argc, char* argv[]) {
         printf("Stress mode enabled - will run until CTRL+C\n\r");
     }
 
-    int pass = 0;
-    for (int n = 0; n < memsize; n++) {
-        unsigned char* ptr = ptrs[n];
+    uint32_t pass = 0;
+    for (uint32_t n = 0; n < memsize; n++) {
+        uint8_t* ptr = ptrs[n];
         printf("Testing memory at %p (%d page)... ", (void*) ptr, n);
-        for (int iter = 0; iter < iterations; iter++) {
-            test_memory((char*)ptr, pgsize, pattern);
+        for (uint32_t iter = 0; iter < iterations; iter++) {
+            test_memory(ptr, pgsize, pattern);
         }
         if (n == memsize-1 && stress_mode) {
             printf("\n\rPass %d: passed!\n\r", pass);
@@ -114,7 +114,7 @@ int main(int argc, char* argv[]) {
         printf("Freeing allocated memory... ");
     }
 
-    for (int n = 0; n < memsize; n++) {
+    for (uint32_t n = 0; n < memsize; n++) {
         if (verbose) {
             printf("\rFreeing allocated memory at %p (%d page)... ", (void*) ptrs[n], n);
         }
--- testmem64.h
+++ testmem64.h
@@ -1,9 +1,11 @@
 #define DEFAULT_MEMSIZE 64
 #define DEFAULT_ITERATIONS 1
 #define DEFAULT_PGSIZE (1024 * 1024)    // 1 MByte
-#define DEFAULT_VERBOSE false
+#define DEFAULT_VERBOSE 0
 #define DEFAULT_PATTERN 'Z'             // ZOV
-#define DEFAULT_STRESSMODE false
+#define DEFAULT_STRESSMODE 0
 #define USAGE_OUT "Usage: %s [-m memory_size] [-i iterations] [-p page_size] [-c pattern] [-v] [-h] [-s]\n\r"
 
-static inline void test_memory(char* mem, int size, char pattern);
+#define eq(a, b) ((a) == (b))
+
+void test_memory(uint8_t* mem, uint32_t size, uint8_t pattern);
